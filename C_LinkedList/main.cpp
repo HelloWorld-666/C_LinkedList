211299807c0c9366ff577f4e71f6374953a922d3
pengmengyuan <pengmengyuan@iboxpay.com>
2019-09-06 15:37:58 +0800

    LeetCode链表相关题

--- C_LinkedList/main.cpp
+++ C_LinkedList/main.cpp
@@ -18,11 +18,12 @@ node* create_link_list()
 	ptr_head->next = NULL;
 
 	ptr_current = ptr_head;
-	int i;
-	for (i = 0; i < 10; i++)
+	int i; 
+	int array[] = { 1, 0, 0 };
+	for (i = 0; i < (sizeof(array)/sizeof(int)); i++)
 	{
 		ptr_new = (node*)malloc(1 * sizeof(node));
-		ptr_new->data = i + 1;
+		ptr_new->data = array[i];// i + 1;
 		ptr_new->next = NULL;
 
 		ptr_current->next = ptr_new;
@@ -97,14 +98,281 @@ void free_link_list(node* ptr_head)
 
 void print_linked_list(node* ptr_head)
 {
-	int i;
+	int i = 0;
 	node* ptr_current = ptr_head->next;
 	while (ptr_current != NULL)
 	{
+		i++;
 		printf("%d\n", ptr_current->data);
 		ptr_current = ptr_current->next;
 	}
-	printf("\n");
+	printf("i=%d\n", i);
+}
+
+// qsort中的比较函数，默认格式如下：
+int compare(const void* a, const void* b)
+{
+	//int a1 = *(int*)a;
+	//int b1 = *(int*)b;
+	return *(int*)a - *(int*)b;	// 如果compar返回值小于0（< 0），那么p1所指向元素会被排在p2所指向元素的前面；
+}
+
+/*
+	思路：
+    1.先统计链表节点个数nums
+    2.再malloc一个nums大小的数组array，并将遍历链表的val赋值到数组中
+    3.利用C库函数快排qsort()对数组array排序
+	4.遍历链表，并将排好序的array[i]赋值给链表每个节点的val
+	// sort-linkedlist
+*/
+node* sortList(node* ptr_head){
+	/*
+	// 冒泡sort：
+	node* p_first = ptr_head;
+	node* p_second = NULL;
+	while (p_first != NULL)
+	{
+		p_second = p_first->next;	// j = i+1
+		while (p_second != NULL)	// j < len
+		{
+			if (p_first->data > p_second->data)
+			{
+				int temp = p_first->data;
+				p_first->data = p_second->data;
+				p_second->data = temp;
+			}
+			p_second = p_second->next;
+		}
+		p_first = p_first->next;
+	}
+	
+	return ptr_head;
+	*/
+
+	// qsort
+	int nums = 0;
+	node* p_cur = ptr_head;
+	while (p_cur != NULL)
+	{
+		nums++;
+		p_cur = p_cur->next;
+	}
+	
+	int* array = (int*)malloc(nums * sizeof(int));
+	int i = 0;
+	p_cur = ptr_head;
+	while (p_cur != NULL)
+	{
+		array[i++] = p_cur->data;
+		p_cur = p_cur->next;
+	}
+	qsort(array, nums, sizeof(int), compare);
+
+	p_cur = ptr_head;
+	i = 0;
+	while (p_cur != NULL)
+	{
+		p_cur->data = array[i++];
+		p_cur = p_cur->next;
+	}
+		
+	return ptr_head;
+}
+
+// reverse linklist
+node* reverseList(node* ptr_head){
+
+	if (NULL == ptr_head || NULL == ptr_head->next)
+	{
+		return ptr_head;
+	}
+
+	node* p1 = ptr_head;
+	node* p2 = p1->next;
+	node* temp = NULL;//node* temp = p2->next;
+	ptr_head->next = NULL;	// p1->next = NULL;
+
+	while (p2 != NULL)
+	{
+		temp = p2->next;// temp = temp->next;
+		p2->next = p1;
+		p1 = p2;
+		p2 = temp;
+		//temp = temp->next;
+	}
+
+	//ptr_head->next = p1;
+
+	free(ptr_head);
+	ptr_head = NULL;
+
+	node* new_head = (node*)malloc(1*sizeof(node));
+	new_head->data = -999;
+	new_head->next = p1;
+
+
+	return new_head;//ptr_head;
+}
+
+/**
+	思路：
+	1.分别遍历两个链表，统计节点数p_count和q_count
+	2.让两条链表中较长的那条链表的指针p从头先走 n=abs(p_count-q_count)步
+	3.最后p和q分别往后遍历，并比较是否有相同的节点，有则返回该节点，无返回NULL
+
+	要考虑的情况如下：
+	1.当两个链表head都为NULL
+	2.两个链表都仅仅只有同一个节点，长度为1
+*/
+node *getIntersectionNode(node *headA, node *headB) {
+	node* p = headA;
+	node* q = headB;
+	int p_count = 0;
+	int q_count = 0;
+
+	if (headA == NULL || headB == NULL)
+	{
+		return NULL;
+	}
+	if (headA == headB)
+	{
+		return headA;
+	}
+
+	while (p != NULL)
+	{
+		p_count++;				
+		p = p->next;
+	}
+	while (q != NULL)
+	{	
+		q_count++;				
+		q = q->next;
+	}
+
+	int n = p_count - q_count;
+	
+	p = headA;
+	q = headB;
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			p = p->next;
+			n--;
+		}
+	}
+	else if (n < 0)
+	{
+		n = abs(n);					//要取绝对值，否则逻辑有问题
+		while (n > 0)
+		{
+			q = q->next;
+			n--;
+		}
+	}
+
+	// n = 0,继续往下执行
+
+	while (p != NULL && q != NULL)
+	{
+		if (p == q)
+		{
+			return p;
+		}
+		p = p->next;
+		q = q->next;
+	}
+
+	return NULL;
+}
+
+bool isPalindrome(struct node* head){
+	// 暴力方法方法1：
+	/*int nums = 0;
+	node* p_curr = head->next;
+	while (p_curr != NULL)
+	{
+		nums++;
+		p_curr = p_curr->next;
+	}
+
+	int* array = (int*)malloc(nums*sizeof(int));
+	int i = 0;
+	p_curr = head->next;
+	while (p_curr != NULL)
+	{
+		array[i++] = p_curr->data;
+		p_curr = p_curr->next;
+	}
+
+	i = 0;
+	while (i <= nums - i - 1)
+	{
+		if (array[i] != array[nums - i - 1])
+		{
+			return false;
+		}
+		i++;
+	}
+
+	return true;*/
+
+
+
+
+	/* 
+		方法2： 反转链表
+		思路：利用快慢指针找到中间，然后将链表的后半部分反转，再依次进行比较
+	*/
+	if (head == NULL)
+	{
+		return false;
+	}
+	if (head->next != NULL && head->next->next == NULL)  // （ Let中则：if (head != NULL || head->next == NULL) ) // 如果只有一个节点
+	{
+		return true;
+	}
+	// 1 2 (2) 1                                   1 2 (1)
+	node* p_slow = head;
+	node* p_fast = head;
+
+	// 找中间位置
+	while (p_fast != NULL && p_fast->next != NULL)		// 头结点的下一个节点不为NULL，相当于除了头之外的第1、2、3....个节点不为NULL（Let中则：p_fast != NULL）
+	{
+		p_slow = p_slow->next;
+		p_fast = p_fast->next->next;
+	}
+
+	// reverse linkedlist后半部分
+	node* p_temp = p_slow->next;						// 备份中间节点:找中间位置，慢指针的下一个节点为中间节点
+	p_fast = p_temp;
+	node* p_fast1 = p_temp->next;
+	node* p_fast2 = NULL;
+
+	p_slow->next = NULL;								// 切断中间位置前的next连接1
+	p_temp->next = NULL;								// 切断中间位置后的next连接2
+
+	while (p_fast1 != NULL)
+	{
+		p_fast2 = p_fast1->next;
+
+		p_fast1->next = p_fast;							// 反向连接
+		p_fast = p_fast1;
+		p_fast1 = p_fast2;
+	}
+
+	p_slow = head->next;								// 不重新定义指针，复用p_slow重新指向头(Let中：p_slow = head)
+	while (p_slow != NULL && p_fast != NULL)
+	{
+		if (p_slow->data != p_fast->data)
+		{
+			return false;
+		}
+		p_slow = p_slow->next;
+		p_fast = p_fast->next;
+	}
+	return true;
 }
 
 int main()
@@ -113,7 +381,7 @@ int main()
 	printf("初始链表:\n");
 	print_linked_list(ptr_head);
 
-	int data = 17;
+	/*int data = 17;
 	int dest_value = 5;
 	
 	node* tmp_insert =insert_linked_list(ptr_head, dest_value, data);
@@ -122,9 +390,38 @@ int main()
 		printf("not find dest_value value.\n");
 	}
 	printf("插入节点后的链表:\n");
-	print_linked_list(ptr_head);
+	print_linked_list(ptr_head);*/
 
-	node* tmp_delete = delete_linked_list(ptr_head, dest_value);
+	/*printf("排序后的链表:\n");
+	node* sort_list = sortList(ptr_head);
+	print_linked_list(sort_list);*/
+
+	//printf("反转后的链表:\n");
+	//node* reverse_list = reverseList(ptr_head);
+	//print_linked_list(reverse_list);
+
+	//node* IntersectionNode = getIntersectionNode(ptr_head, ptr_head);
+	//if (IntersectionNode == NULL)
+	//{
+	//	printf("无相交节点");
+	//}
+	//else
+	//{
+	//	printf("相交的的节点:\n");
+	//}
+	//print_linked_list(IntersectionNode);
+
+	bool b = isPalindrome(ptr_head);
+	if (b == true)
+	{
+		printf("是\n");
+	}
+	else
+	{
+		printf("否\n");
+	}
+
+	/*node* tmp_delete = delete_linked_list(ptr_head, dest_value);
 	if (tmp_delete == NULL)
 	{
 		printf("not find dest_value value.\n");
@@ -132,7 +429,7 @@ int main()
 	printf("删除节点后的链表:\n");
 	print_linked_list(ptr_head);
 
-	free_link_list(ptr_head);
+	free_link_list(ptr_head);*/
 
 	getchar();
 	return 0;
